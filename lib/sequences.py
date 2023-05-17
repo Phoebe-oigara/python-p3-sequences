e7eb876372bdde1cf23291fd86a4329824dc687e
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-17 21:46:26 +0300

    succesful implementation of fibonacci at attempt 2

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,15 +1,24 @@
 def print_fibonacci(length):
-    fibonacci_list = []
-    
-    if length <= 0:
-        print([])
-        return
-    
-    #  first two numbers in the sequence
 
-    if length >= 1:
-        fibonacci_list.append(0)
-        print(fibonacci_list[0])
+    fibonacci_list = []
+    a, b = 0, 1
+
+    for _ in range (length):
+        fibonacci_list.append(a)
+        a, b = b, a + b
     
-    # Generating the remaining numbers in the sequence
+    print(fibonacci_list)
+
+
+        # method 2
+
+        # Generating the Fibonacci sequence
+    # for i in range(length):
+    #     if i == 0:
+    #         fibonacci_list.append(0)
+    #     elif i == 1:
+    #         fibonacci_list.append(1)
+    #     else:
+    #         fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
     
+    # # Printing the Fibonacci sequence
