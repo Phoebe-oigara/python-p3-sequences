50fd35b9afd1a81d78dd244b7108449f8adc1146
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-17 20:11:08 +0300

    attempt 1 at fibonacci, tests failing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    
+    if length <= 0:
+        print([])
+        return
+    
+    #  first two numbers in the sequence
+
+    if length >= 1:
+        fibonacci_list.append(0)
+        print(fibonacci_list[0])
+    
+    # Generating the remaining numbers in the sequence
+    
